99d2f4f0b1bb6a67f26291abd454b7a28ca1a21b
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-04-27 10:38:45 +0600

    handle sudo

--- plugin.zsh
+++ plugin.zsh
@@ -1,8 +1,18 @@
 add-params() {
-    BIN=${BUFFER%% *}
-    COUNT=$((${#BIN}+1))
-    CURSOR=$COUNT
-    # hack for adding yet space before params
+    WORDS=(${(z)BUFFER})
+    if [ $#WORDS -lt 2 ]; then
+        return 1
+    fi
+
+    BIN=$BUFFER
+    SHIFT=2 # this one for the extra space
+    if [ $#WORDS -gt 2 ]; then
+        BIN=${BIN#sudo }
+        SHIFT=$((LEN + 5))
+    fi
+
+    BIN=${BIN%% *}
+    CURSOR=$((${#BIN} + ${SHIFT}))
     if [[ $CURSOR -gt 0 ]]
     then
       BUFFER[$CURSOR]="  "
